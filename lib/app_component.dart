abf92fa55358d7b461dee77f60281c792c40ba4f
cfleur <cfleur@users.noreply.github.com>
2018-10-16 20:38:47 +0300

    add hero list and service called asynchronously

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,36 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
+import 'dart:async';
+//import 'package:angular_forms/angular_forms.dart';
+
 import 'src/hero.dart';
+import 'src/mock_heroes.dart';
+import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [formDirectives],
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, HeroComponent],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+
+class AppComponent implements OnInit {
   final title = 'Hero Cats Tour';
-  Hero hero = Hero(1, 'Cafe');
+  List<Hero> heroes;
+  Hero selected;
+
+  final HeroService _heroService;
+  AppComponent(this._heroService);
+
+  void ngOnInit() => _getHeroes();
+
+  Future<void> _getHeroes() async {
+//    Future.then() method, use with void:
+//    _heroService.getAll().then((heroes) => this.heroes = heroes);
+    heroes = await _heroService.getAll();
+  }
+
+  void onSelect(Hero hero) => selected = hero;
+
 }
